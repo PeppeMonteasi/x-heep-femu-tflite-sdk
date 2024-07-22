256b31dd423a6cff460c9c07fe3b955d2f855409
PeppeMonteasi <g.monteasi@studenti.unina.it>
2024-07-22 18:46:02 +0200

    added adversial functions

--- sw/riscv/apps/tflite_scpi/lenet5_test.cc
+++ sw/riscv/apps/tflite_scpi/lenet5_test.cc
@@ -91,3 +91,104 @@ extern "C" int init_tflite() {
 extern "C" int infer(const char *data, size_t len, int8_t **out, size_t *out_len) {
   return Infer(data, len, out, out_len);
 }
+
+
+// Calcolo della loss 
+int calculate_loss(const int8_t *label, const int8_t *output, size_t len) {
+    int loss = 0;
+    for (size_t i = 0; i < len; i++) {
+        int diff = label[i] - output[i];
+        loss += diff * diff;
+    }
+    return loss;
+}
+
+// Calcolo del gradiente 
+void calculate_gradient(const int8_t *original_image, const int8_t *label, int8_t *gradient, size_t len) {
+    int8_t perturbed_image[len];
+    int8_t *output;
+    size_t output_len;
+
+    Infer((const char *)original_image, len, &output, &output_len);
+    int original_loss = calculate_loss(label, output, output_len);
+
+
+    memcpy(perturbed_image, original_image, len);
+    for (size_t i = 0; i < len; i++) {
+        perturbed_image[i] += 1; 
+        Infer((const char *)perturbed_image, len, &output, &output_len);
+        int new_loss = calculate_loss(label, output, output_len);
+        if(new_loss - original_loss > 0)
+          gradient[i] = 1;
+        else
+          gradient[i] = -1;
+    }
+}
+
+
+
+void fgsm_attack(const int8_t *original_image, const int8_t *label, int8_t *adversarial_image, int epsilon) {
+
+  int8_t adv_image[lenet_input_data_size];
+  memcpy(adv_image, original_image, lenet_input_data_size);
+
+  //inference
+  int8_t *output;
+  size_t output_len;
+  Infer((const char *)adv_image, lenet_input_data_size, &output, &output_len);
+
+  int8_t gradient[lenet_input_data_size];
+  memset(gradient, 0, lenet_input_data_size);
+
+  // Calcolo del gradiente facendo la derivata della loss rispetto all'immagine
+   calculate_gradient(original_image, label, gradient, lenet_input_data_size);
+
+
+  // Create the adversarial image
+  for (size_t i = 0; i < lenet_input_data_size; i++) {
+    adversarial_image[i] = original_image[i] + ((epsilon)>>4 * gradient[i]);
+    if (adversarial_image[i] < 0) adversarial_image[i] = 0;
+    if (adversarial_image[i] > 127) adversarial_image[i] = 127;
+  }
+}
+
+
+void fgsm_attack_with_shift(const int8_t *original_image, const int8_t *label, int8_t *adversarial_image, int epsilon) {
+
+
+    int8_t shifted_image[lenet_input_data_size];
+    int8_t gradient[lenet_input_data_size];
+    int extra_bits[lenet_input_data_size] = {0};
+    int8_t shifted_grad[lenet_input_data_size];
+    int extra_bits_grad[lenet_input_data_size] = {0};
+
+
+
+    
+    calculate_gradient(original_image, label, gradient, lenet_input_data_size);
+
+    for (size_t i = 0; i < lenet_input_data_size; i++) {
+        extra_bits[i] = original_image[i] 
+        extra_bits[i] = extra_bits[i]>>(8-4);
+        extra_bits[i] = extra_bits[i]<<(8-4);
+
+        shifted_image[i] = original_image[i] 
+        shifted_image[i] = shifted_image[i]<<(4);
+
+        extra_bits_grad[i] = gradient[i] 
+        extra_bits_grad[i] = extra_bits_grad[i]>>(8-4);
+        extra_bits_grad[i] = extra_bits_grad[i]<<(8-4);
+
+        shifted_grad[i] = gradient[i] 
+        shifted_grad[i] = shifted_grad[i]<<(4);
+
+
+
+        adversarial_image[i] = shifted_image[i] + ((epsilon) * shifted_grad[i]);
+        adversarial_image[i] = adversarial_image[i] + extra_bits[i] + ((epsilon<<4) * extra_bits_grad[i]);
+
+        if (adversarial_image[i] < 0) adversarial_image[i] = 0;
+        if (adversarial_image[i] > 127) adversarial_image[i] = 127;
+        
+    }
+} 
