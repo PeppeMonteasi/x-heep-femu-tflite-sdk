57819d212f76ed59dbe2946a640ea348d901187e
PeppeMonteasi <g.monteasi@studenti.unina.it>
2024-07-24 18:30:27 +0200

    cambiare directory

--- sw/arm/apps/tflite_scpi/tflite_scpi.py
+++ sw/arm/apps/tflite_scpi/tflite_scpi.py
@@ -1,4 +1,5 @@
 from pynq import x_heep
+import os
 
 # Estendi la classe x_heep per aggiungere debug
 class x_heep_debug(x_heep):
@@ -15,6 +16,8 @@ class x_heep_debug(x_heep):
         super().run_app()
         print("Application execution complete")
 
+
+os.chdir("home/g.monteasi/x-heep-femu-tflite-sdk/")
 # Load the X-HEEP bitstream
 x_heep_dbg = x_heep_debug()
 
